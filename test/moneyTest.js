60330e26d02c890587cdfce4f767795dcd8f8d8c
Subrata Das <subratadas5112000@gmail.com>
2024-07-30 09:56:57 +0530

    tested the currencyFomat

---
+++ test/moneyTest.js
@@ -0,0 +1,31 @@
+import { currencyFormat } from "../scripts/utils/money.js";
+
+console.log('test suite: Currency format');
+
+console.log(' the code works in cents ');
+
+if(currencyFormat(2095) === '20.95') {
+    console.log('Passed');
+}
+else {
+    console.log('Failed');
+}
+
+console.log('work with 0');
+
+if(currencyFormat(0) === '0.00') {
+    console.log('passed');
+}
+else {
+    console.log('failed');
+}
+
+console.log('work with round with cents');
+
+if(currencyFormat(2000.5) === '20.01') {
+    console.log('passed');
+}
+else {
+    console.log('failed');
+}
+
